351215b5d5d49261485fc0a841e3fb0199d9e56f
midgerate <midgerate@gmail.com>
2022-01-07 15:18:33 +0530

    fix(): return object instead of array to avoid lint errors

--- src/hooks/useWriteContract.tsx
+++ src/hooks/useWriteContract.tsx
@@ -2,11 +2,11 @@ import {
   Contract,
   ContractReceipt,
   ContractTransaction,
-} from '@ethersproject/contracts'
+} from '@ethersproject/contracts';
 
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react';
 
-import { ContractFunctions, ContractInstance } from './types'
+import { ContractFunctions, ContractInstance } from './types';
 export const useWriteContract = <
   TContract extends ContractInstance = any,
   TFunctionName extends string & keyof ContractFunctions<TContract> = string
@@ -14,24 +14,22 @@ export const useWriteContract = <
   contract: TContract | Contract | null,
   functionName: TFunctionName,
   options?: {
-    confirmations?: number
-    onError?: (error: Error) => void
-    onResponse?: (response: ContractTransaction) => void
-    onConfirmation?: (receipt: ContractReceipt) => void
+    confirmations?: number;
+    onError?: (error: Error) => void;
+    onResponse?: (response: ContractTransaction) => void;
+    onConfirmation?: (receipt: ContractReceipt) => void;
   }
-): [
-  (
+): {
+  mutate: (
     ...args: Parameters<ContractFunctions<TContract>[TFunctionName]>
-  ) => Promise<void>,
-  {
-    error: Error | null
-    response: ContractTransaction | undefined
-  }
-] => {
-  const [error, setError] = useState<Error | null>(null)
+  ) => Promise<void>;
+  error: Error | null;
+  response: ContractTransaction | undefined;
+} => {
+  const [error, setError] = useState<Error | null>(null);
   const [response, setResponse] = useState<ContractTransaction | undefined>(
     undefined
-  )
+  );
 
   useEffect(() => {
     const cleanup = () => {
@@ -39,48 +37,48 @@ export const useWriteContract = <
         return {
           error: new Error('No contract provided'),
           response: undefined,
-        }
+        };
       }
 
       return {
         error: null,
         response: undefined,
-      }
-    }
+      };
+    };
 
-    cleanup()
-  }, [(contract as Contract)?.signer, functionName])
+    cleanup();
+  }, [(contract as Contract)?.signer, functionName]);
 
   const mutate = useCallback(
     async (
       ...args: Parameters<ContractFunctions<TContract>[TFunctionName]>
     ) => {
       if (!contract) {
-        const error = new Error('No contract provided')
-        options?.onError?.(error)
-        setError(error)
+        const error = new Error('No contract provided');
+        options?.onError?.(error);
+        setError(error);
       }
       try {
         const response: ContractTransaction = await (contract as Contract)[
           functionName
-        ](...args)
-        options?.onResponse?.(response)
-        setResponse(response)
-        const receipt = await response.wait(options?.confirmations ?? undefined)
-        options?.onConfirmation?.(receipt)
+        ](...args);
+        options?.onResponse?.(response);
+        setResponse(response);
+        const receipt = await response.wait(
+          options?.confirmations ?? undefined
+        );
+        options?.onConfirmation?.(receipt);
       } catch (error) {
-        options?.onError?.(error as Error)
-        setError(error as Error)
+        options?.onError?.(error as Error);
+        setError(error as Error);
       }
     },
     [(contract as Contract)?.signer, functionName]
-  )
+  );
 
-  return [
+  return {
     mutate,
-    {
-      error,
-      response,
-    },
-  ]
-}
+    error,
+    response,
+  };
+};
